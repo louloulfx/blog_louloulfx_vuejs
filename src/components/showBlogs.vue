2f2e7cc2a5ace46000c4aac3d916f2a8a0a16f70
louloulfx <louloulfx1@hotmail.fr>
2019-03-23 17:51:38 +0100

    leçon 34, custom devices (création de v-qqchose comme on veut)

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="show-blogs">
+  <div v-theme:column="'narrow'" id="show-blogs">
     <h1>Voici tout les articles disponibles</h1>
     <div v-for="blog in blogs" :key="blog" class="single-blog">
-        <h2>{{blog.title}}</h2>
+        <h2 v-rainbow>{{blog.title}}</h2>
         <article>{{blog.body}}</article>
     </div>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,36 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueResource from 'vue-resource'
+import Vue from "vue";
+import App from "./App.vue";
+import VueResource from "vue-resource";
 
 Vue.use(VueResource);
 
+//Customs directives
+Vue.directive("rainbow", {
+  bind(el, binding, vnode) {
+    el.style.color =
+      "#" +
+      Math.random()
+        .toString()
+        .slice(2, 8);
+  }
+});
+
+Vue.directive("theme", {
+  bind(el, binding, vnode) {
+    if (binding.value == "wide") {
+      el.style.maxWidth = "1200px";
+    } else if (binding.value == "narrow") {
+      el.style.maxWidth = "560px";
+    }
+    if (binding.arg == 'column'){
+      el.style.background = '#ddd';
+      el.style.padding = '20px'
+    }
+  }
+});
+
 export const bus = new Vue();
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
