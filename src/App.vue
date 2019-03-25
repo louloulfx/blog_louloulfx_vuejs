0a5d26dad9b56b38fce1e5577a4301efc2312619
louloulfx <louloulfx1@hotmail.fr>
2019-03-25 13:12:57 +0100

    n°41: navbar avec liens pour les routes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
@@ -8,11 +9,13 @@
 import addBlog from "./components/addBlog";
 import showBlogs from "./components/showBlogs";
 import listBlogs from "./components/listBlogs";
+import header from "./components/entete";
 export default {
   components: {
     "add-blog": addBlog,
     "show-blogs": showBlogs,
-    "list-blogs": listBlogs
+    "list-blogs": listBlogs,
+    "app-header": header
   },
   data() {
     return {};
